45052a158fbd7ff9074442e804f057d482b9e8b8
Danyliz <jdaniel.ls.cc@gmail.com>
2024-02-13 18:14:59 -0600

    se agrega el contenido de la app en app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+
+st.header('Proyecto Sprint 5')
+hist_button = st.checkbox('Construir histograma')  # crear un botón
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+
+# crear botón de gráfico de dispersión
+disper_button = st.checkbox('Construir gráfico de dispersión')
+
+if disper_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.scatter(car_data, x="odometer", y="price")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
